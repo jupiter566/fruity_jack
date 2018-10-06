56e3bcac668cc78c75864ea68c1874c52aa6f75b
Jupiter566 <kinesos4@gmail.com>
2018-10-06 05:31:28 +0200

    fenetre du prout

--- Hello.java
+++ Hello.java
@@ -1,5 +1,19 @@
-public class Hello {
+
+import javax.swing.*;
+import java.awt.*;
+public class Hello extends JFrame {
     public static void main(String[] arg) {
-	    System.out.println("Hello prout");
+		JFrame f = new JFrame ("fenetre du prout");
+		f.setDefaultCloseOperation(Hello.EXIT_ON_CLOSE);
+		f.setSize(666,333);
+		f.setVisible(true);
+		f.setLocationRelativeTo(null);
+		JPanel prout = new JPanel();
+		prout.setBackground(Color.BLUE);
+		f.setContentPane(prout);
+		f.setVisible(true);
+		System.out.println("Fin");
+	
     }
+	
 }
