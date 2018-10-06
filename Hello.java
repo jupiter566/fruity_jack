2f8fd061d6d7f8ae33c7b7e58c190654f8c192cd
Jupiter566 <kinesos4@gmail.com>
2018-10-06 05:44:18 +0200

    ajout du prout dans sa fenêtre

--- Hello.java
+++ Hello.java
@@ -5,15 +5,18 @@ public class Hello extends JFrame {
     public static void main(String[] arg) {
 		JFrame f = new JFrame ("fenetre du prout");
 		f.setDefaultCloseOperation(Hello.EXIT_ON_CLOSE);
-		f.setSize(666,333);
+		f.setSize(999,333);
 		f.setVisible(true);
 		f.setLocationRelativeTo(null);
 		JPanel prout = new JPanel();
-		prout.setBackground(Color.BLUE);
-		f.setContentPane(prout);
+		prout.setBackground(Color.white);
+		
 		f.setVisible(true);
 		System.out.println("Fin");
-	
+        prout.setLayout(new FlowLayout());
+		JLabel label = new JLabel("Le prout");
+        prout.add(label);
+	    f.setContentPane(prout);
     }
 	
 }
